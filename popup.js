edf7740c6debf149837d20dc4ee554d687ef93bb
Maria Eduarda Weiland Machado Bratti <weiland.maria@edu.pucrs.br>
2025-05-31 14:41:48 -0300

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,1 +1,12 @@
-console.log('This is a popup!');
+document.getElementById('openApp').addEventListener('click', async () => {
+  try {
+    const res = await fetch('http://localhost:3000', { method: 'HEAD' });
+    if (res.ok) {
+      chrome.tabs.create({ url: 'http://localhost:3000' });
+    } else {
+      alert('Servidor local não está respondendo.');
+    }
+  } catch (error) {
+    alert('Não foi possível conectar ao servidor local.');
+  }
+});
